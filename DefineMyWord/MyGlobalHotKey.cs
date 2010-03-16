fbe284fecdf7cc106dbdd79542fc4ce5a42749b7
SundaraRaman <sundaryourfriend@gmail.com>
2010-03-16 18:22:18 +0530

    Adding GNU GPL license and copyright notices.
    It's good to be thorough. :)

--- DefineMyWord/MyGlobalHotKey.cs
+++ DefineMyWord/MyGlobalHotKey.cs
@@ -1,4 +1,25 @@
+/*
+    Copyright (c) 2010 SundaraRaman Ramalingam
 
+    This file is part of DefineMyWord.
+
+    DefineMyWord is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DefineMyWord is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DefineMyWord.  If not, see <http://www.gnu.org/licenses/>.
+ 
+    The classes KeyboardHook, KeyPressedEventArgs, and the enum ModifierKeys were 
+    originally published by Christian Liensberger at http://www.liensberger.it/web/blog/?p=207.
+     
+*/
 using System;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
